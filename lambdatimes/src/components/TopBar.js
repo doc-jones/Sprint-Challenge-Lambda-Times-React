f5473ba83316f80f74b2e6a205109d1a9c4e547c
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-05-03 13:43:04 -0400

    Add partial styled-components to TopBar

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -4,7 +4,29 @@ import styled, { css } from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const TopBarFull = styled.div`
+width:100%;
+display:flex;
+justify-content:center;
+align-items:none;
+flex-direction:row;
+position:fixed;
+height:44px;
+background-color: #333;
+`;
 
+const TopBarContainer= styled.div`
+ width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+@media(min-width: 1280px){
+  width:1280px;
+}`;
 
 
 const TopBar = () => {
