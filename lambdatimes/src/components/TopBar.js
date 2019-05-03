106e37230ddb9134827a1c790b31b0a9801bfe4f
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-05-03 13:54:07 -0400

    Add styled-components to TopBar

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -4,7 +4,7 @@ import styled, { css } from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
-const TopBarFull = styled.div`
+const TB = styled.div`
 width:100%;
 display:flex;
 justify-content:center;
@@ -15,7 +15,7 @@ height:44px;
 background-color: #333;
 `;
 
-const TopBarContainer= styled.div`
+const TBContainer= styled.div`
  width: 100%;
   display: flex;
   justify-content: none;
@@ -27,23 +27,66 @@ const TopBarContainer= styled.div`
 @media(min-width: 1280px){
   width:1280px;
 }`;
+const TBLeft = styled.div`
+display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  span{
+    cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+  }
+`;
+const TBCenter = styled.div`
+display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+  span {
+     cursor: pointer;
+  margin-right: 5%;
+  }
+  span:last-child {
+    margin-right:0;
+  }
+  span:hover{
+    text-decoration:underline;
+  }
+`;
+const TBRight = styled.div`
+display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+  span {
+    cursor:pointer;
+  }
+`;
 
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
+    <TB>
+      <TBContainer>
+        <TBLeft>
           <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
+        </TBLeft>
+        <TBCenter>
           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+        </TBCenter>
+        <TBRight>
           <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+        </TBRight>
+        </TBContainer>
+      </TB>
   )
 }
 
