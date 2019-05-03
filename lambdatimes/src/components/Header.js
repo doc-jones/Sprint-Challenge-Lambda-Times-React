0a669a92285720c9ab3659d8eec56e758dcd0700
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-05-03 13:22:51 -0400

    Add styled-components to Header

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,7 +1,50 @@
 import React from 'react';
 
+import styled, { css } from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+
+const beginHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+  @media (min-width: 1280px) {
+    .header{
+      width:1280px;
+    }
+    h1 {
+      font-size: 60px;
+      font-family: Didot, serif;
+      font-weight: bold;
+      flex: 8;
+      text-align: center;
+      color: #000;
+    }
+    span {
+      align-self: flex-end;
+      font-size: 11px;
+      font-weight: bold;
+      letter-spacing: 1px;
+    }
+  `
+    const headerDate = styled,span`
+      margin-left: 25px;
+      flex: 1;
+    `
+    const headerTemp = styled.span`
+      text-align: right;
+      margin-right: 25px;
+      flex: 1;
+  `
+
+
 
 const Header = () => {
   return (
@@ -13,4 +56,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
